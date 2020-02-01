82f3288987e631858e25e0eb17b16b03b7327eba
spin6lock <spin6lock@users.noreply.github.com>
2020-02-02 01:50:37 +0800

    prepare blog templates for me :)

--- compose.py
+++ compose.py
@@ -5,13 +5,46 @@
 This module use fd-find to find out latest modified post, and fix the space problem.
 """
 
+import os
 import subprocess
+import datetime
+import argparse
 
-def main():
-    subprocess.call(["fd", ".", "_posts/", "--changed-within=1day", "-x", "python3", "post_add_space/main.py"]) 
+def handle_args():
+    parser = argparse.ArgumentParser(description="accept blog title as argument")
+    parser.add_argument("title", help="the title of the new blogpost")
+    args = parser.parse_args()
+    return args.title
+
+def prepare_file(title):
+    datestr = f"{datetime.datetime.now():%Y-%m-%d}"
+    post_filename = ''.join([datestr, '-', title, '.md'])
+    front_matter = f"""---
+layout:     post
+title:      "{title}"
+subtitle:   ""
+date:       {datetime.datetime.now():%Y-%m-%d}
+author:     "spin6lock"
+catalog:    true
+tags:
+---
+"""
+    filepath = os.path.join("_posts", post_filename) 
+    with open(filepath, "w") as fh:
+        fh.write(front_matter)
+    EDITOR = os.environ.get("EDITOR", "vim")
+    subprocess.call([EDITOR, filepath])
+
+def git_commit_push():
     subprocess.call(["git", "add", "_posts/*.md"]) 
     subprocess.call(["git", "commit", "-v"]) 
     subprocess.call(["git", "push"])
+
+def main():
+    title = handle_args()
+    prepare_file(title)
+    subprocess.call(["fd", ".", "_posts/", "--changed-within=5min", "-x", "python3", "post_add_space/main.py"]) 
+    git_commit_push()
 
 if __name__ == "__main__":
     main()
